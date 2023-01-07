7baad5479629086ab84f41fc4f65b427949a6ff4
Your Name <you@example.com>
2023-01-08 02:34:17 +0300

    in

--- NewsPortal/qwe/templatetags/custom_filters.py
+++ NewsPortal/qwe/templatetags/custom_filters.py
@@ -1,16 +1,31 @@
 from django import template
+from pprint import pprint
+
+
+class StrError(Exception):
+    def __str__(self):
+        return 'Error'
 
 
 register = template.Library()
 
-qwe = ['UIO', 'f']
+qwe = {'uio': '***',
+       'f': '*'}
+
 
 @register.filter()
 def censor(value):
-   postfix = ''
-   for i in value.split():
-      if i in qwe:
-         i = '*'
-      postfix += f'{i} '
+    try:
+        if type(value) is not str:
+            raise StrError()
+    except StrError as e:
+        pprint(e)
+        return
+    postfix = ''
+    for i in value.split():
+        if i.lower() in qwe:
+            i = qwe[i.lower()]
+        postfix += f'{i} '
 
-   return f'{postfix}'
+    return f'{postfix}'
+
