0b8b827b4d3f022fe6c62c1cab1f81855e394fe4
Your Name <you@example.com>
2023-01-26 17:21:17 +0300

    in

--- NewsPortal/qwe/urls.py
+++ NewsPortal/qwe/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.urls import path
-from .views import PostList, NewsCreate, ArticleCreate, PostUpdate, NewsList, ArticleList, NewsDetail, ArticleDetail, PostDelete
+from .views import PostList, PostDetail, NewsCreate, ArticleCreate, PostUpdate, NewsList, ArticleList, NewsDetail, ArticleDetail, PostDelete
 
 
 urlpatterns = [
    path('post/', PostList.as_view(), name='post_list'),
    path('news/', NewsList.as_view(), name='news_list'),
    path('article/', ArticleList.as_view(), name='article_list'),
-   path('post/<pk>', NewsDetail.as_view(), name='post_detail'),
+   path('post/<pk>', PostDetail.as_view(), name='post_detail'),
    path('news/<pk>', NewsDetail.as_view(), name='news_detail'),
    path('article/<pk>', ArticleDetail.as_view(), name='article_detail'),
    path('news/create/', NewsCreate.as_view(), name='news_create'),
